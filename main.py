53a14d5bdd6ddd41d5a4ca97b13fab5495f3e1b6
Misha Shetty <mishashetty11@gmail.com>
2026-06-05 12:49:18 +0530

    dcfvbghn

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') 
+def home():
+    return "Hello, World!"
+
+app.route("/abc")
+def abc():
+    return "This is the ABC route."
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
